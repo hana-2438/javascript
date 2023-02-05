80fae063965c29ae4cea60723222b70b11378d73
EC2 Default User <ec2-user@ip-172-31-11-216.ap-northeast-1.compute.internal>
2023-02-05 06:58:58 +0000

    [Finish]javascript-2

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
-let hello = "Hello world";
-alert(hello);
-alert(4+3);
+// let hello = "Hello world";
+// alert(hello);
+// let number = 4+3;
+// alert(number);
+// let greeting = "こんにちは";
+// let name = "はな";
+// alert(greeting + name);
+
+
+
+// let orange = 200;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while (num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+let num1 = 5;
+let num2 = 6;
+
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert(num1 / num2);
